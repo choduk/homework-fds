3188c85de8cc897b9ecc637bb599762b78b89092
choduk <choduk88@gmail.com>
2018-01-20 01:20:06 +0900

    initialize commit

---
+++ src/main/java/homework/fds/FdsApplication.java
@@ -0,0 +1,12 @@
+package homework.fds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdsApplication.class, args);
+	}
+}
---
+++ src/test/java/homework/fds/FdsApplicationTests.java
@@ -0,0 +1,16 @@
+package homework.fds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FdsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
