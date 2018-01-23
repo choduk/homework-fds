6cd74cb6b395053f69b9811e226d731acd7460b0
choduk <choduk88@gmail.com>
2018-01-24 04:41:15 +0900

    [logic] Rule Engine Thread처리, Exception 추가

--- engine/src/main/java/homework/fds/FraudDetectionEngine.java
+++ engine/src/main/java/homework/fds/FraudDetectionEngine.java
@@ -1,10 +1,12 @@
 package homework.fds;
 
+import homework.fds.exception.RuleMatchRuntimeException;
 import homework.fds.rule.Rule;
 import homework.fds.log.UserActionLog;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -14,15 +16,74 @@ import static java.util.stream.Collectors.toList;
  */
 public class FraudDetectionEngine {
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime()
+                                                                                        .availableProcessors());
     private final Collection<Rule> rules;
 
     public FraudDetectionEngine(Collection<Rule> rules) {
         this.rules = rules;
     }
 
-    public List<Rule> findMatchedRules(List<UserActionLog> actionUserActionLogs) {
-        return rules.stream()
-                    .filter(rule -> rule.isMatch(actionUserActionLogs))
-                    .collect(toList());
+    public List<Rule> findMatchedRules(List<UserActionLog> userActionLogs) {
+
+        List<Future<RuleResult>> futureList = rules.stream()
+                                                   .map(rule -> executorService.submit(new RuleCallable(rule, userActionLogs)))
+                                                   .collect(toList());
+
+        return futureList.stream()
+                         .map(this::ruleMatching)
+                         .filter(RuleResult::isSuccess)
+                         .map(RuleResult::getRule)
+                         .collect(toList());
+
+    }
+
+    private RuleResult ruleMatching(Future<RuleResult> future) {
+        try {
+            return future.get(2L, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuleMatchRuntimeException(e);
+        }
+    }
+
+    class RuleCallable implements Callable<RuleResult> {
+
+        final Rule rule;
+        final List<UserActionLog> userActionLogs;
+
+        public RuleCallable(Rule rule, List<UserActionLog> userActionLogs) {
+            this.rule = rule;
+            this.userActionLogs = userActionLogs;
+        }
+
+        @Override
+        public RuleResult call() throws Exception {
+            return new RuleResult(rule, rule.isMatch(userActionLogs));
+        }
+    }
+
+    class RuleResult {
+
+        final Rule rule;
+        final boolean success;
+
+        public RuleResult(Rule rule, boolean success) {
+            this.rule = rule;
+            this.success = success;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public Rule getRule() {
+            return rule;
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        executorService.shutdownNow();
+        super.finalize();
     }
 }
---
+++ engine/src/main/java/homework/fds/exception/RuleMatchRuntimeException.java
@@ -0,0 +1,11 @@
+package homework.fds.exception;
+
+/**
+ * @author choduk88@sk.com
+ * @since 2018. 1. 24..
+ */
+public class RuleMatchRuntimeException extends RuntimeException {
+    public RuleMatchRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
