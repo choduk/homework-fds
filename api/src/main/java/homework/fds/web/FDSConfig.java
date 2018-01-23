cd31174f0aaf6d1ed710c6286256b52626a5b203
choduk <choduk88@gmail.com>
2018-01-24 03:17:52 +0900

    [logic] FDS 설정 추가

--- api/src/main/java/homework/fds/web/FDSConfig.java
+++ api/src/main/java/homework/fds/web/FDSConfig.java
@@ -1,10 +1,21 @@
 package homework.fds.web;
 
 import homework.fds.FraudDetectionEngine;
+import homework.fds.filter.AccountOpenPeriodCondition;
+import homework.fds.filter.ActionTypeCondition;
+import homework.fds.filter.Condition;
+import homework.fds.filter.PeriodCondition;
+import homework.fds.rule.KakaoPayRule;
+import homework.fds.rule.Rule;
+import homework.fds.type.ActionType;
+import homework.fds.validator.KakaoMoneyReceiveValidator;
+import homework.fds.validator.KakaoRuleACompositeValidator;
+import homework.fds.validator.RuleValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author choduk88@sk.com
@@ -14,7 +25,30 @@ import java.util.Collections;
 public class FDSConfig {
 
     @Bean
-    public FraudDetectionEngine fraudDetectionEngine() {
-        return new FraudDetectionEngine(Collections.emptyList());
+    public FraudDetectionEngine fraudDetectionEngine(List<Rule> ruleList) {
+        return new FraudDetectionEngine(ruleList);
+    }
+
+    @Bean
+    public Rule ruleA() {
+        Condition condition = new AccountOpenPeriodCondition(1);
+        RuleValidator ruleValidator = new KakaoRuleACompositeValidator();
+        return new KakaoPayRule("RuleA", condition, ruleValidator);
+    }
+
+    @Bean
+    public Rule ruleB() {
+        Condition condition = new AccountOpenPeriodCondition(24 * 7);
+        RuleValidator ruleValidator = new KakaoMoneyReceiveValidator(100000L, 5);
+        return new KakaoPayRule("RuleB", condition, ruleValidator);
+    }
+
+    @Bean
+    public Rule ruleC() {
+        Condition periodCondition = new PeriodCondition(LocalDateTime.now()
+                                                                     .minusHours(2L), LocalDateTime.now());
+        periodCondition.next(new ActionTypeCondition(ActionType.RECEIVE));
+        RuleValidator validator = new KakaoMoneyReceiveValidator(50000L, 3);
+        return new KakaoPayRule("RuleC", periodCondition, validator);
     }
 }
