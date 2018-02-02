29e52b7c4290dac64588854dcb8f4630c6e218db
choduk <choduk88@gmail.com>
2018-02-03 04:49:47 +0900

    [feature] Rule에 next 제거

--- engine/src/main/java/homework/fds/core/Rule.java
+++ engine/src/main/java/homework/fds/core/Rule.java
@@ -6,7 +6,6 @@ import homework.fds.validator.RuleValidator;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * @author choduk88@sk.com
@@ -16,24 +15,14 @@ public class Rule {
 
     private final RuleFilter filter;
     private final RuleValidator validator;
-    private Optional<Rule> next = Optional.empty();
 
     public Rule(RuleFilter filter, RuleValidator validator) {
         this.filter = Objects.requireNonNull(filter);
         this.validator = Objects.requireNonNull(validator);
     }
 
-    public Rule setNext(Rule next) {
-        this.next = Optional.of(Objects.requireNonNull(next));
-        return next;
-    }
-
     public boolean isMatch(LogRawData logRawData) {
         List<ActionLog> filteredActionLogList = filter.doFilter(logRawData);
-
-        if(!validator.valid(filteredActionLogList))
-            return false;
-
-        return !next.isPresent() || next.get().isMatch(logRawData);
+        return validator.valid(filteredActionLogList);
     }
 }
